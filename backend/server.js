d815aa0948cb6c2faedcd8fecdba54e82a5eb52c
LettttttDuelll <phamdung07102004@gmail.com>
2025-10-16 09:57:04 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -10,27 +10,27 @@ const app = express();
 import cors from "cors";
 app.use(bodyParser.json());
 app.use(cors());
+
 app.post("/crawl", async (req, res) => {
     const { url } = req.body;
     if (!url) {
         return res.status(400).json({ error: "URL is required" });
     }
+    try {
+        const data = await main(url); //
+        res.json({ success: true, message: data });
+        res.json(data);
+    }
+    catch (err) {
+        console.error("Crawler error:", err);
+        res.status(500).json({ error: "Crawler failed" });
+    };
 });
-
-try {
-    const data = await main(url); //
-    res.json({ success: true, message: data });
-    res.json(data);
-}
-catch (err) {
-    console.error("Crawler error:", err);
-    res.status(500).json({ error: "Crawler failed" });
-};
 
 app.listen(8080, () => { console.log("Server running at http://localhost:8080"); });
 
 cron.schedule("0 6 * * *", async () => {
     await main("https://vnexpress.net");//await để đợi hàm main hoàn thành 
-    await main1(); 
+    await main1();
     console.log("🔄 Cron job executed at 6:00 AM");
 })
